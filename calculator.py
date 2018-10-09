fa971db66c6bf2f3f500028d35a850a1c639c4f0
Jesus España <jesusespana5023@gmail.com>
2018-10-09 20:23:30 +0200

    Basic calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,17 @@
-print "Hello world"
-print"HOLA"
+x = int(raw_input("Enter the value of x: "))
+y = int(raw_input("Enter the value of y: "))
+
+operation = raw_input("Enter the operation you want to do: ")
+
+if operation == "+":
+    print x + y
+elif operation == "-":
+    print x - y
+elif operation == "*":
+    print x * y
+elif operation == "/":
+    print x / y
+else:
+    print "operation isn't correct"
+
+
